6386203a5991a14d873965357320634915d6731d
harsh-909 <harshchauhan.go@gmail.com>
2025-11-05 18:20:02 +0530

    changed baseName

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/my-portfolio">
+  <BrowserRouter basename="/myPortfolio">
     <App />
   </BrowserRouter>
 );
